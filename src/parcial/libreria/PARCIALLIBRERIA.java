f810619f6a0145e732290b966b6cbef3c27aad6a
Gtarquicode <>
2025-05-08 15:07:28 -0500

    Ingreso de usuarios en la libreria principal

---
+++ src/parcial/libreria/PARCIALLIBRERIA.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package parcial.libreria;
+ 
+
+
+/*Como primer paso es necesario que se tenga el registro de los nuevos usuarios
+            sea docente o estudiante
+            */
+public class PARCIALLIBRERIA {
+
+    /**
+     *
+     */
+    public class Usuario{
+        private string usuario;
+        private string libros;
+        private string catalogo;
+    }
+
+    
+}
