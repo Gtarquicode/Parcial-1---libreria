76727708a15094ff4ba5d0f6112cfcfba8363a75
juane <juane@LAPTOP-87OUM3TE>
2025-05-19 17:33:25 -0500

    Agregué clase usuario

--- BibliotecaUniversitaria/src/parcial/libreria/Usuario.java
+++ BibliotecaUniversitaria/src/parcial/libreria/Usuario.java
@@ -7,7 +7,31 @@ package parcial.libreria;
 /**
  *
  * @author juane
- */
+ */ 
+// Creo la clase usuario para representar a quienes usan el espacio de la biblioteca
 public class Usuario {
-    
+    // Atributos del usuario
+    private String nombre; //Nombre
+    private String tipo; //Tipo de usuario
+
+    // Constructor
+    public Usuario(String nombre, String carrera) {
+        this.nombre = nombre;
+        this.tipo = carrera;
+    }
+
+    // Métodos getter
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    // El método para mostrar la información del usuario
+    @Override
+    public String toString() {
+        return "Usuario: " + nombre + " | Carrera: " + tipo;
+    }
 }
